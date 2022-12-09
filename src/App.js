e5642690b4365029063a3ff56b377b630e08459b
Valentin Korneev <v.korneef@gmail.com>
2022-12-09 12:16:11 +0600

    homework complete cards created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Cards from './components/Card';
+import Card from './components/Card';
 import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
 
-      <Cards title='Component with image' text='This component have an image'>
+      <Card title='Component with image' text='This component have an image'>
         <img src={logo} className="card-img-top" alt="some" />
-      </Cards>
+      </Card>
 
-      <Cards title='Component without image' text='This component not have an image'/>
+      <Card title='Component without image' text='This component not have an image'/>
 
     </div>
   );
